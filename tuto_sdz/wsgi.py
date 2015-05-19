6d78d5e98166603414536211ef314161c7a09893
Ro-Ran <lcudini@gmail.com>
2015-05-20 00:18:13 +0200

    ajout projet

---
+++ tuto_sdz/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tuto_sdz project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tuto_sdz.settings")
+
+application = get_wsgi_application()
