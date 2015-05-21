024e18b5c4099f30f2d99c359809c679dbc54051
Laurent Cudini <lcudini@gmail.com>
2015-05-21 14:34:42 +0200

    fin chap 1.1 - les vues

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('blog.views',
-	url(r'^$', 'home'),
+	url(r'^$', 'home', name="home"),
 	url(r'^accueil$', 'home'),
+	url(r'^article/(?P<id_article>\d+)$', 'view_article', name="show_article"),
+	url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})$', 'list_articles', name="list_articles"),
+	url(r'^what-is-django$', 'what_is_django', name="django"),
+	url(r'^redirect-home$', 'test_redirect_home', name="redirect-home"),
+	url(r'^redirect-42$', 'test_redirect_42', name="redirect-42"),
 )
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 def home (request):
@@ -8,3 +8,29 @@ def home (request):
 	text = """<h1>Bienvenue sur le blog tuto-sdz</h1>
 		<p>The cake is a lie...</p>"""
 	return HttpResponse(text)
+
+def view_article(request, id_article):
+	"""Vue qui prend en paramètre une variable id_article"""
+
+	#Si l'id est inférieur à 1, nous considerons que l'article n'existe pas
+	if int(id_article) < 1:
+		raise Http404
+
+	text = "Vous avez demandé l'article #{0} !".format(id_article)
+	return HttpResponse(text)
+
+def list_articles(request, year, month):
+	""" Liste des articles d'un mois précis"""
+
+	text = "Vous avez demandé les articles du {0}/{1}".format(month, year)
+	return HttpResponse(text)
+
+def what_is_django(request):
+	#Redirige l'utilisateur vers le site de django
+	return redirect("https://www.djangoproject.com")
+
+def test_redirect_home(request):
+	return redirect(home)
+
+def test_redirect_42(request):
+	return redirect('show_article', id_article=42)
